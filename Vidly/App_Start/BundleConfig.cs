3fb97331bd78dd88193cbeca91e9db06e2daad2c
ezempel <edward.zempel@gmail.com>
2020-07-06 16:20:40 -0500

    Use lumen theme
    Must be the bootstrap version 3 compatible version

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Vidly
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap3-lumen.css",
                       "~/Content/site.css"));
         }
     }
