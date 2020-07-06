a35541c452a7376b0646cea3156746672dc521e8
ezempel <edward.zempel@gmail.com>
2020-07-06 16:35:12 -0500

    Types of ActionResult

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -19,5 +19,27 @@ namespace Vidly.Controllers
             //return new EmptyResult();
             // return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
     }
 }
